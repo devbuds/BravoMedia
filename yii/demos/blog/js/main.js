bcd695ec9d64b386e9080c907f5da8987dc43037
Syed Hussain <imran2324@hotmail.com>
2016-04-12 17:35:22 +0200

    creating new js file

---
+++ yii/demos/blog/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+
+// make the  my saved tutorial bar disappear when user click on search button for searching the tutorials
+$(document).on('click', '.img-responsive', function(){
+	
+	alert('ok we got');
+});
