6bf2db0871604cda016000ba9bd75f93f45df3fc
Syed Hussain <imran2324@hotmail.com>
2016-04-12 12:50:33 +0200

    adding html for product page

--- yii/demos/blog/protected/views/site/product.php
+++ yii/demos/blog/protected/views/site/product.php
@@ -1,2 +1,30 @@
 <?php
-echo 'hello';
+foreach ($result as $row) {
+
+    ?>
+<!-- div containing one product with all its properties-->
+<div  itemtype="http://schema.org/Product"  class="productListItem odd">
+
+        <meta content="<?= CHtml::encode( $row['name'] ) ?>" itemprop="name">
+        <div class="CSthumbnail ">
+        
+                  <!--	image container -->
+           		<div class="imageLink">
+                		<div class="imageWrapper">
+                    <a href="#">
+                        <img alt="lifevest image"  src="<?= Yii::app()->request->getBaseUrl(true).'/images/'.CHtml::encode( $row['image'] )?>"   class="img-responsive block-center" itemprop="image">
+                    </a>
+                 	</div>
+            		 </div>
+            		 
+                <!--  Title and shoping button container -->
+                <div itemscope="" itemprop="offers" class="caption">
+                    <meta content="<?= CHtml::encode( $row['name'] ) ?>" itemprop="product title">
+                    <p class="productTitle"><a class="bold" href="#"><?= CHtml::encode( $row['name'] ) ?></a></p>
+                    <p class="köpbutton"><a role="button"  itemprop="url" href="#">SHOP NOW</a></p>               	   
+                </div>
+               
+        </div>       
+</div>
+    
+<?php  } ?>
