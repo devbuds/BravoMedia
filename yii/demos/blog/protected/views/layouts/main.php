56986d0bf7c34833990683c226c58038e374c480
Syed Hussain <imran2324@hotmail.com>
2016-04-12 12:07:08 +0200

    adding product page in navigation and removing about, contact, login

--- yii/demos/blog/protected/views/layouts/main.php
+++ yii/demos/blog/protected/views/layouts/main.php
@@ -29,6 +29,7 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('post/index')),
+			    array('label'=>'Product', 'url'=>array('site/product')),
 				array('label'=>'About', 'url'=>array('site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('site/contact')),
 				array('label'=>'Login', 'url'=>array('site/login'), 'visible'=>Yii::app()->user->isGuest),
