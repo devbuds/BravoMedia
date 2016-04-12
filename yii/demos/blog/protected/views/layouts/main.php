627bcad6480af85c076588437d78dc6da00c3dfd
Syed Hussain <imran2324@hotmail.com>
2016-04-12 12:10:17 +0200

    removing about, contact, login from navigation

--- yii/demos/blog/protected/views/layouts/main.php
+++ yii/demos/blog/protected/views/layouts/main.php
@@ -30,10 +30,6 @@
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('post/index')),
 			    array('label'=>'Product', 'url'=>array('site/product')),
-				array('label'=>'About', 'url'=>array('site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('site/contact')),
-				array('label'=>'Login', 'url'=>array('site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
 		)); ?>
 	</div><!-- mainmenu -->
