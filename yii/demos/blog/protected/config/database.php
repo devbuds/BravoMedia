af0dcd15ac32dfbcb8ff2c12bb9b19542293c7a4
Syed Hussain <imran2324@hotmail.com>
2016-04-12 11:02:00 +0200

    adding database configuration file

---
+++ yii/demos/blog/protected/config/database.php
@@ -0,0 +1,10 @@
+<?php
+// This is the database connection configuration.
+return array(
+	'connectionString' => 'mysql:host=localhost;dbname=bravomedia',
+	'emulatePrepare' => true,
+	'username' => 'syehu564',
+	'password' => 'root',
+	'charset' => 'utf8',
+
+);
